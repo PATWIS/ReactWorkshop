7645440d7ed7822046563fb17155016cfaba662e
PATWIS <patryk.wisniewski@outlook.com>
2018-02-18 00:53:31 +0100

    added fav list

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,25 @@
 import 'bootstrap/scss/bootstrap.scss';
 import './scss/main.scss'
 import { data } from './data';
-import { EventCard } from './cardEvent.jsx';
-
-
-let list = data.slice(0, 3);
-
-const EventsList = (list)=> (
-    <div>
-      {list
-        .sort((a, b) => a.date > b.date)
-        .map((e) => <EventCard data={e} key={e.id}/>)}
-    </div>
-  )
-
-ReactDom.render(EventsList(list), document.getElementById('app'));
+import { EventsList, FavEventsList } from './cardEvent.jsx';
 
 
 
+let list = [], page = 1, perpage = 2;
+
+document.querySelector('#showMore').addEventListener('click', () => { page++; update() })
+
+let favEventsList = data.slice(0, 1);
+
+
+function update() {
+  let count = page * perpage;
+  list = data.slice(0, count);
+  ReactDom.render(<div>
+   <FavEventsList list={favEventsList} />
+    <EventsList list={list} />
+  </div>, document.getElementById('app'));
+}
+
+
+update();
--- src/cardEvent.jsx
+++ src/cardEvent.jsx
@@ -1,33 +1,48 @@
 import FaStar from 'react-icons/lib/fa/star';
 import FaStarO from 'react-icons/lib/fa/star-o';
+
+
 const EventDetails = ({ data }) => {
   return (
-    <table className="table" style={{ width: 200 }}>
-      <tbody>
-        <tr>
-          <th>When</th>
-          <td>{data.date}</td>
-        </tr>
-        <tr>
-          <th>Where</th>
-          <td>{data.city}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div>
+      <table className="table" style={{ width: 200 }}>
+        <tbody>
+          <tr>
+            <th>When</th>
+            <td>{data.date}</td>
+          </tr>
+          <tr>
+            <th>Where</th>
+            <td>{data.city}</td>
+          </tr>
+        </tbody>
+      </table>
+      <FavButton isFavourite={false} />
+    </div>
   )
 }
 
-const Button = ({icon, label}) => (<button className="btn btn-default">{icon} {label}</button>)
+const FavDetails = ()=> (
+  <FavButton isFavourite={true}/>
+)
 
 const EventActions = () => (
   <div className="btn-group d-flex float-sm-right" role="group">
     <Button label='Sign in' />
-    <Button label='Add to favourite' icon={<FaStar/>}/>
+    <Button label='Add to favourite' icon={<FaStar />} />
   </div>
 )
 
-export const EventCard = (props) => {
-  let { data } = props;
+const Button = props => (<button className="btn btn-outline-primary" {...props}>{props.icon} {props.label}</button>)
+
+const FavButton = ({ className = "btn btn-sm btn-block", label, icon, isFavourite }) => (isFavourite
+  ? <Button className={className + " btn-outline-danger"} icon={icon || <FaStar />} label={label || 'Remove from favourite'} />
+  : <Button className={className + " btn-outline-success"} icon={icon || <FaStarO />} label={label || 'Add to favourite'} />
+)
+
+
+const EventCard = (props) => {
+  let { data, Details } = props;
   return (
     <div className="card">
       <div className="card-body">
@@ -39,10 +54,10 @@ export const EventCard = (props) => {
             <h5 className="mt-0">{data.name}</h5>
             <p className="badge badge-primary">{data.category}</p>
             <p>{data.desc}</p>
-            <EventActions />
+            {props.children}
           </div>
           <div className="media-right">
-            <EventDetails {...props} />
+            <Details {...props} />
           </div>
         </div>
       </div>
@@ -50,3 +65,27 @@ export const EventCard = (props) => {
 
   )
 }
+
+export const EventsList = ({list}) => (
+  <div>
+    <h2>Upcoming events</h2>
+    <hr />
+    <div>
+      {list
+        .sort((a, b) => a.date > b.date)
+        .map((e) => <EventCard data={e} key={e.id} Details={EventDetails}> <EventActions /> </EventCard>)}
+    </div>
+  </div>
+)
+
+export const FavEventsList = ({list}) => (
+  <div>
+    <h2>Favourite events</h2>
+    <hr />
+    <div>
+      {list
+        .sort((a, b) => a.date > b.date)
+        .map((e) => <EventCard data={e} key={e.id} Details={FavDetails}/>)}
+    </div>
+  </div>
+)
